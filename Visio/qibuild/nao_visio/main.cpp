f228b03073a06d01f20e017148cd598c483c7c8d
rouviecy <rouvcyril@yahoo.fr>
2013-11-10 00:54:59 +0100

    NAO will say hello world

--- Visio/qibuild/nao_visio/main.cpp
+++ Visio/qibuild/nao_visio/main.cpp
@@ -4,9 +4,24 @@
  * found in the COPYING file.
  */
 #include <iostream>
+#include <alerror/alerror.h>
+#include <alproxies/altexttospeechproxy.h>
 
-int main()
-{
-  std::cout << "Hello, world" << std::endl;
-  return 0;
+int main(int argc, char* argv[]){
+	if(argc != 2){
+		std::cerr << "Wrong number of arguments!" << std::endl;
+		std::cerr << "Usage: say NAO_IP" << std::endl;
+		exit(2);
+	}
+
+	  /** The desired phrase to be said. */
+	const std::string phraseToSay = "Hello world";
+	try{
+		AL::ALTextToSpeechProxy tts(argv[1], 9559);
+		tts.say(phraseToSay);
+	}catch(const AL::ALError& e){
+		std::cerr << "Caught exception: " << e.what() << std::endl;
+		exit(1);
+	}
+	exit(0);
 }
