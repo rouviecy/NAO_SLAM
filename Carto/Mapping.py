0fae684e2bfb1cccffb2696c5aa41fcdb38fa8b6
TheoBlanchard <theo.blanchard@ensta-bretagne.org>
2013-12-06 23:54:06 +0100

    Update Mapping.py

--- Carto/Mapping.py
+++ Carto/Mapping.py
@@ -19,28 +19,35 @@ def CreateMap(CoefMap):
 ##CoorObstacle de la Forme: [xDebut yDebut ; xFin yFin]    
 
 
-def DrawObstacle(CoorObstacle):
+ef DrawObstacle(CoefMap, CoorObstacle):
    
     
-    tmpl=[k for k in range (CoorObstacle[1][1] + 1 -CoorObstacle[0][1])]
+    tmpl=[k for k in range (CoefMap)]
 
     for line_num, line in enumerate(fileinput.input('Map',inplace=1)):  
-        if (CoorObstacle[0][1]<= line_num <= CoorObstacle[1][1]):
             tmp = line
-            tmpl[line_num-CoorObstacle[0][1]] = tmp.split()
+            tmpl[line_num] = tmp.split()
             j = CoorObstacle[0][0]
-            while (j <= CoorObstacle[1][0]):
-                    tmpl[line_num-CoorObstacle[0][1]][j] = '1'
-                    j = j + 1
-            new = ' '.join(tmpl[line_num-CoorObstacle[0][1]])
-            new = new + ' \n'
-            line = line.replace(tmp, new)
             
-        print line
+            if (CoorObstacle[0][1]<= line_num <= CoorObstacle[1][1]):
+                while (j <= CoorObstacle[1][0]):
+                    tmpl[line_num][j] = '1'
+                    j = j + 1
+            new = ' '.join(tmpl[line_num])
+            line = line.replace(tmp, new)
+            print line
 ## Exemple pour voir comment ça marche.        
 if __name__ == "__main__":
 
-    
-    CreateMap(100)
-    obstacle = [[0,0],[10, 10]]
-    DrawObstacle(obstacle)
+    a=100
+    CreateMap(a)
+    obstacle  = [[00,00],[10, 10]]
+    obstacle2 = [[10,10],[20, 20]]
+    obstacle3 = [[20,20],[30, 30]]
+    obstacle4 = [[30,30],[40, 40]]
+    obstacle5 = [[40,40],[50, 50]]
+    DrawObstacle(a, obstacle)
+    DrawObstacle(a, obstacle2)
+    DrawObstacle(a, obstacle3)
+    DrawObstacle(a, obstacle4)
+    DrawObstacle(a, obstacle5)
