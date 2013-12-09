793602adea6c2f1c728ffccf72cda558a53a7429
TheoBlanchard <theo.blanchard@ensta-bretagne.org>
2013-12-09 10:18:48 +0100

    Update Capteur.py

--- Hardware/Capteur.py
+++ Hardware/Capteur.py
@@ -1,16 +1,16 @@
 import time
-from Naoqi import *
+from naoqi import *
 
 IP = "127.0.0.1"
 
 memoryProxy = ALProxy("ALMemory", IP, 9559)
-sonarProxy = ALProxy("ALSonar", ip, 9559)
-
+sonarProxy = ALProxy("ALSonar", IP, 9559)
+sensorProxy = ALProxy("ALSensors", IP, 9559)
 def getBatteryCharge():
     return memoryProxy.getData("Device/SubDeviceList/Battery/Current/Sensor/Value")
 
 
-def SonarLeftRight():    
+def SonarLeftRight():
     sonarProxy.subscribe("myApplication")
     leftFirstEcho  = memoryProxy.getData("Device/SubDeviceList/US/Left/Sensor/Value")
     RightFirstEcho = memoryProxy.getData("Device/SubDeviceList/US/Right/Sensor/Value")
@@ -18,7 +18,7 @@ def SonarLeftRight():
 
 
 def Sonar():
-    
+
     sonarProxy.subscribe("myApplication")
     return memoryProxy.getData("Device/SubDeviceList/US/Sensor/Value")
 
@@ -34,31 +34,51 @@ def Piezo():
     RrearLeft  = memoryProxy.getData("Device/SubDeviceList/RFoot/FSR/RearLeft/Sensor/Value")
     RrearRight = memoryProxy.getData("Device/SubDeviceList/RFoot/FSR/RearRight/Sensor/Value")
     Rtotal     = memoryProxy.getData("Device/SubDeviceList/RFoot/FSR/TotalWeight/Sensor/Value")
-    
+    Total = (Ltotal+Rtotal)/2
+    return Total
+
 def Temperature():
-    TempList =[TBatt           = memoryProxy.getData("Device/SubDeviceList/Battery/Temperature/Sensor/Value"),
-    THeadPitch      = memoryProxy.getData("Device/SubDeviceList/HeadPitch/Temperature/Sensor/Value"),
-    THeadYaw        = memoryProxy.getData("Device/SubDeviceList/HeadYaw/Temperature/Sensor/Value"),
-    TChevilleGPitch = memoryProxy.getData("Device/SubDeviceList/LAnklePitch/Temperature/Sensor/Value"),
-    TChevilleGRoll  = memoryProxy.getData("Device/SubDeviceList/LAnkleRoll/Temperature/Sensor/Value"),
-    TCoudeGRoll     = memoryProxy.getData("Device/SubDeviceList/LElbowRoll/Temperature/Sensor/Value"),
-    TCoudeGYaw      = memoryProxy.getData("Device/SubDeviceList/LElbowYaw/Temperature/Sensor/Value"),
-    TMainG          = memoryProxy.getData("Device/SubDeviceList/LHand/Temperature/Sensor/Value"),
-    THancheGPitch   = memoryProxy.getData("Device/SubDeviceList/LHipPitch/Temperature/Sensor/Value"),
-    THancheGRoll    = memoryProxy.getData("Device/SubDeviceList/LHipRoll/Temperature/Sensor/Value"),
-    THancheGYaw     = memoryProxy.getData("Device/SubDeviceList/LHipYawPitch/Temperature/Sensor/Value"),
-    TGenouGPitch    = memoryProxy.getData("Device/SubDeviceList/LKneePitch/Temperature/Sensor/Value"),
-    TEpauleGPitch   = memoryProxy.getData("Device/SubDeviceList/LShoulderPitch/Temperature/Sensor/Value"),
-    TEpauleGRoll    = memoryProxy.getData("Device/SubDeviceList/LShoulderRoll/Temperature/Sensor/Value"),
-    TPoignetGYaw    = memoryProxy.getData("Device/SubDeviceList/LWristYaw/Temperature/Sensor/Value"),   
-    TChevilleDPitch = memoryProxy.getData("Device/SubDeviceList/RAnklePitch/Temperature/Sensor/Value"),
-    TChevilleDRoll  = memoryProxy.getData("Device/SubDeviceList/RAnkleRoll/Temperature/Sensor/Value"),
-    TCoudeDRoll     = memoryProxy.getData("Device/SubDeviceList/RElbowRoll/Temperature/Sensor/Value"),
-    TCoudeDYaw      = memoryProxy.getData("Device/SubDeviceList/RElbowYaw/Temperature/Sensor/Value"),
-    TMainD          = memoryProxy.getData("Device/SubDeviceList/RHand/Temperature/Sensor/Value"),
-    THancheDPitch   = memoryProxy.getData("Device/SubDeviceList/RHipPitch/Temperature/Sensor/Value"),
-    THancheDRoll    = memoryProxy.getData("Device/SubDeviceList/RHipRoll/Temperature/Sensor/Value"),
-    TGenouDPitch    = memoryProxy.getData("Device/SubDeviceList/RKneePitch/Temperature/Sensor/Value"),
-    TEpauleDPich    = memoryProxy.getData("Device/SubDeviceList/RShoulderPitch/Temperature/Sensor/Value"),
-    TEpauleDRoll    = memoryProxy.getData("Device/SubDeviceList/RShoulderRoll/Temperature/Sensor/Value"),
-    TPoignetDYaw    = memoryProxy.getData("Device/SubDeviceList/RWristYaw/Temperature/Sensor/Value")]
+    TBatt           = memoryProxy.getData("Device/SubDeviceList/Battery/Temperature/Sensor/Value")
+    THeadPitch      = memoryProxy.getData("Device/SubDeviceList/HeadPitch/Temperature/Sensor/Value")
+    THeadYaw        = memoryProxy.getData("Device/SubDeviceList/HeadYaw/Temperature/Sensor/Value")
+    TChevilleGPitch = memoryProxy.getData("Device/SubDeviceList/LAnklePitch/Temperature/Sensor/Value")
+    TChevilleGRoll  = memoryProxy.getData("Device/SubDeviceList/LAnkleRoll/Temperature/Sensor/Value")
+    TCoudeGRoll     = memoryProxy.getData("Device/SubDeviceList/LElbowRoll/Temperature/Sensor/Value")
+    TCoudeGYaw      = memoryProxy.getData("Device/SubDeviceList/LElbowYaw/Temperature/Sensor/Value")
+    TMainG          = memoryProxy.getData("Device/SubDeviceList/LHand/Temperature/Sensor/Value")
+    THancheGPitch   = memoryProxy.getData("Device/SubDeviceList/LHipPitch/Temperature/Sensor/Value")
+    THancheGRoll    = memoryProxy.getData("Device/SubDeviceList/LHipRoll/Temperature/Sensor/Value")
+    THancheGYaw     = memoryProxy.getData("Device/SubDeviceList/LHipYawPitch/Temperature/Sensor/Value")
+    TGenouGPitch    = memoryProxy.getData("Device/SubDeviceList/LKneePitch/Temperature/Sensor/Value")
+    TEpauleGPitch   = memoryProxy.getData("Device/SubDeviceList/LShoulderPitch/Temperature/Sensor/Value")
+    TEpauleGRoll    = memoryProxy.getData("Device/SubDeviceList/LShoulderRoll/Temperature/Sensor/Value")
+    TPoignetGYaw    = memoryProxy.getData("Device/SubDeviceList/LWristYaw/Temperature/Sensor/Value")
+    TChevilleDPitch = memoryProxy.getData("Device/SubDeviceList/RAnklePitch/Temperature/Sensor/Value")
+    TChevilleDRoll  = memoryProxy.getData("Device/SubDeviceList/RAnkleRoll/Temperature/Sensor/Value")
+    TCoudeDRoll     = memoryProxy.getData("Device/SubDeviceList/RElbowRoll/Temperature/Sensor/Value")
+    TCoudeDYaw      = memoryProxy.getData("Device/SubDeviceList/RElbowYaw/Temperature/Sensor/Value")
+    TMainD          = memoryProxy.getData("Device/SubDeviceList/RHand/Temperature/Sensor/Value")
+    THancheDPitch   = memoryProxy.getData("Device/SubDeviceList/RHipPitch/Temperature/Sensor/Value")
+    THancheDRoll    = memoryProxy.getData("Device/SubDeviceList/RHipRoll/Temperature/Sensor/Value")
+    TGenouDPitch    = memoryProxy.getData("Device/SubDeviceList/RKneePitch/Temperature/Sensor/Value")
+    TEpauleDPich    = memoryProxy.getData("Device/SubDeviceList/RShoulderPitch/Temperature/Sensor/Value")
+    TEpauleDRoll    = memoryProxy.getData("Device/SubDeviceList/RShoulderRoll/Temperature/Sensor/Value")
+    TPoignetDYaw    = memoryProxy.getData("Device/SubDeviceList/RWristYaw/Temperature/Sensor/Value")
+    Tmat = [TBatt, THeadPitch, THeadYaw, TChevilleGPitch, TChevilleGRoll, TCoudeGRoll, TCoudeGYaw, TMainG, THancheGRoll, THancheGPitch, TGenouGPitch, THancheGYaw, TGen$
+    return Tmat
+
+def bumperPressed():
+
+        LeftBump = [(memoryProxy.getData("Device/SubDeviceList/LFoot/Bumper/Left/Sensor/Value")==1.0),(memoryProxy.getData("Device/SubDeviceList/LFoot/Bumper/Right/Sen$
+        RightBump= [(memoryProxy.getData("Device/SubDeviceList/RFoot/Bumper/Left/Sensor/Value")==1.0),(memoryProxy.getData("Device/SubDeviceList/RFoot/Bumper/Right/Sen$
+        BumpMat  = [LeftBump, RightBump]
+        return BumpMat
+
+
+if __name__ == "__main__":
+        print(getBatteryCharge())
+        print(SonarLeftRight())
+        print(Sonar())
+        print(Temperature())
+        print(bumperPressed())
+        print(piezo())
